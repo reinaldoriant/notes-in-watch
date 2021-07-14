1bfd14cbae6e62d25d95c9447e8d56a04f3bfa24
Reinaldo <reinaldo.riant@kompas.com>
2021-07-14 09:50:16 +0700

    + add navigation link

--- NotesWatch WatchKit Extension/View/ContentView.swift
+++ NotesWatch WatchKit Extension/View/ContentView.swift
@@ -89,13 +89,16 @@ struct ContentView: View {
             if notes.count>=1 {
                 List{
                     ForEach(0..<notes.count, id: \.self){i in
-                        HStack{
-                            Capsule()
-                                .frame(width: 4)
-                                .foregroundColor(.accentColor)
-                            Text(notes[i].text)
-                                .lineLimit(1)
-                                .padding(.leading,5)
+                        NavigationLink(
+                            destination: DetailView(note: notes[i], count: notes.count, index: i)){
+                            HStack{
+                                Capsule()
+                                    .frame(width: 4)
+                                    .foregroundColor(.accentColor)
+                                Text(notes[i].text)
+                                    .lineLimit(1)
+                                    .padding(.leading,5)
+                            }
                         }//: Hstack
                     } //:Loop
                     .onDelete(perform:
