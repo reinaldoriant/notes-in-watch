a8d807cd852af5953a73b3c638fae1ee9a6082b5
Reinaldo <reinaldo.riant@kompas.com>
2021-07-14 09:57:53 +0700

    + add credits

---
+++ NotesWatch WatchKit Extension/View/CreditsView.swift
@@ -0,0 +1,27 @@
+//
+//  CreditsView.swift
+//  NotesWatch WatchKit Extension
+//
+//  Created by TI Digital on 14/07/21.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    //MARK : - Property
+    
+    // MARK : - Body
+    var body: some View {
+        VStack (spacing: 3){
+           //Profile image
+            //header
+            
+        }
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+    }
+}
