fb8ca20f1a3cce11f515ace15d90064bf573670c
Reinaldo <reinaldo.riant@kompas.com>
2021-07-14 10:35:47 +0700

    ! show credits

--- NotesWatch WatchKit Extension/View/DetailView.swift
+++ NotesWatch WatchKit Extension/View/DetailView.swift
@@ -14,6 +14,8 @@ struct DetailView: View {
     let count: Int
     let index: Int
     
+    @State private var isCreditsPresented: Bool = false
+    
     var body: some View {
         VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 3) {
            
@@ -41,6 +43,12 @@ struct DetailView: View {
                 
                 Image(systemName: "info.circle")
                     .imageScale(.large)
+                    .onTapGesture {
+                        isCreditsPresented.toggle()
+                    }
+                    .sheet(isPresented: $isCreditsPresented, content: {
+                        CreditsView()
+                    })
             }
             .foregroundColor(.secondary)
         }
