bf7a62ad8b775d2e3f8b937eb535d8a61aba392f
Reinaldo <reinaldo.riant@kompas.com>
2021-07-14 09:35:12 +0700

    + add func delete + add background when data is empty

--- NotesWatch WatchKit Extension/View/ContentView.swift
+++ NotesWatch WatchKit Extension/View/ContentView.swift
@@ -50,6 +50,13 @@ struct ContentView: View {
         }
     }
     
+    func delete(offsets: IndexSet){
+        withAnimation{
+            notes.remove(atOffsets: offsets)
+            save()
+        }
+    }
+    
     // MARK : - Body
     var body: some View {
         VStack {
@@ -79,18 +86,30 @@ struct ContentView: View {
             }//:HSTACK
             Spacer()
             
-            List{
-                ForEach(0..<notes.count, id: \.self){i in
-                    HStack{
-                        Capsule()
-                            .frame(width: 4)
-                            .foregroundColor(.accentColor)
-                        Text(notes[i].text)
-                            .lineLimit(1)
-                            .padding(.leading,5)
-                    }//: Hstack
-                    
+            if notes.count>=1 {
+                List{
+                    ForEach(0..<notes.count, id: \.self){i in
+                        HStack{
+                            Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+                            Text(notes[i].text)
+                                .lineLimit(1)
+                                .padding(.leading,5)
+                        }//: Hstack
+                    } //:Loop
+                    .onDelete(perform:
+                        delete)
                 }
+            } else {
+                Spacer()
+                Image(systemName: "note.text")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.gray)
+                    .opacity(0.25)
+                    .padding(25)
+                Spacer()
             }
         }//: Vstack
         
