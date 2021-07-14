8b8771c020af76b91d425163bef96e6acd018fe7
Reinaldo <reinaldo.riant@kompas.com>
2021-07-14 10:31:36 +0700

    + add credits ! modify image

--- NotesWatch WatchKit Extension/View/CreditsView.swift
+++ NotesWatch WatchKit Extension/View/CreditsView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct CreditsView: View {
     //MARK : - Property
+    @State private var randomNumber: Int = Int.random(in: 1..<4)
     
+    private var randomImage: String {
+        return "developer-no\(randomNumber)"
+    }
     // MARK : - Body
     var body: some View {
         VStack (spacing: 3){
            //Profile image
+            Image(randomImage)
+                .resizable()
+                .scaledToFit()
+                .layoutPriority(1)
             //header
-            
+            HeaderView(title: "Credits")
+            //content
+            Text("Reinaldo Riant")
+                .foregroundColor(.primary)
+                .fontWeight(.bold)
+        
+            Text("Developer")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .fontWeight(.light)
         }
     }
 }
